8fc73c7faf9507c1247ef139566d861c039de158
MykhailoHumeniuk <gumeniukmich@gmail.com>
2025-08-22 14:15:19 +0300

    fix

--- src/main/java/michhmk/blogapp/repository/user/PostRepository.java
+++ src/main/java/michhmk/blogapp/repository/user/PostRepository.java
@@ -1,12 +1,11 @@
 package michhmk.blogapp.repository.user;
 
+import java.util.Optional;
 import michhmk.blogapp.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     @EntityGraph(attributePaths = "author")
